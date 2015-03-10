2c9d4dde14f9fd5a1e7220ee890ec7128ed78322
Christopher Burkey <cburkey@openedit.org>
2015-03-10 17:46:35 -0400

    grid!

---
+++ webapp/em-mobile/components/javascript/mobile.js
@@ -0,0 +1,122 @@
+
+$(document).ready(function(){/* off-canvas sidebar toggle */
+
+$('[data-toggle=offcanvas]').click(function() {
+  	$(this).toggleClass('visible-xs text-center');
+    $(this).find('i').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
+    $('.row-offcanvas').toggleClass('active');
+    $('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
+    $('#xs-menu').toggleClass('visible-xs').toggleClass('hidden-xs');
+    $('#btnShow').toggle();
+});
+
+loadInto = function(inLink,cell)
+{
+	jQuery.get(inLink, {}, function(data) 
+	{
+		cell.html(data);
+	});
+}
+
+jQuery('.playerclink').bind('click',function(e)
+{
+	e.preventDefault();
+	var link = $(this);
+
+	var hidden = $("#hiddenoverlay");
+
+	loadInto(link.attr("href") + "&oemaxlevel=1",hidden);
+
+	//Now show overlay
+	hidden.show();
+	
+});
+
+	jQuery('.addfilter').bind('click',function(e)
+	{	
+		e.preventDefault();
+		//TODO: Why not update the results with new data like VD does?
+			var link = $(this);
+			var picked = link.data("filtertype");
+			jQuery('#filtertype').val(picked);
+			var value = link.data("filtervalue");
+			jQuery('#filtervalue').val(value);
+			var label = link.data("filterlabel");
+			jQuery('#filterlabel').val(label);
+
+			jQuery('#dontshow').submit();
+			
+	});
+
+	$(document).on('click',"#closebutton",function(e)
+	{	
+		e.preventDefault();
+		var hidden = $("#hiddenoverlay");
+		hidden.hide();
+	});
+	
+	$(document).on('click',"#playbutton",function(e)
+			{	
+				e.preventDefault();
+				var div = $(this);
+				div.removeClass("glyphicon-play");
+				div.addClass("glyphicon-pause");
+				console.log("Now Play slideshow");
+			});
+
+	
+});
+
+function doResize() 
+{
+	var fixedheight = 300;
+	var sofarused = 0;
+	var totalavailable = $(".masonry-grid ").width(); 
+	var row = [];
+	$(".masonry-grid .masonry-grid-cell").each(function()
+			
+		var cell = $(this);
+		var w = cell.data("width");
+		if( !w )
+		{
+			w = 100;
+		}
+		
+		var h = cell.data("height");
+		if( !h )
+		{
+			h = 100;
+		}
+		w = parseInt(w);
+		h = parseInt(h);
+		var a = w / h;  
+		
+		var wimg = Math.floor( fixedheight * a );
+		
+		sofarused = sofarused + neww;
+		if( sofarused > totalavailable )
+		{
+			//TODO: set the height of this row
+			var overage = totalavailable / sofarused;
+			var newheight = fixedheight * overage;
+			$.each( row, function()
+				
+			);
+		}
+		else
+		{
+			sofarused = neww;			
+			row.add( cell );
+		}
+		
+		
+	);
+	
+	
+	
+
+}
+
+$(window).resize(function(){
+	doResize();
+});
